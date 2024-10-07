24ce31ef45323d70520e100b73bdc9fe3afaa3dd
tominaga <zmstmb2804@ymobile.ne.jp>
2024-10-07 17:07:24 +0900

    corsの追加

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,11 @@ import authRoutes from './routes/auth';
 import ticketRoutes from './routes/tickets';
 import inventoryRoutes from './routes/inventory';
 import orderRoutes from './routes/orders';
+import cors from 'cors';
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // 各ルートを登録
 app.use('/api/auth', authRoutes);
