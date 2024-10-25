1f497908698b309f49355f5332232e0beb7877b4
tominaga <zmstmb2804@ymobile.ne.jp>
2024-10-25 22:19:10 +0900

    Update Firebase configuration to use environment variables

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,21 +1,14 @@
-// src/config/firebaseConfig.ts
-
-import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import * as path from 'path';
-import * as fs from 'fs';
 
-// サービスアカウントキーのパス
-const serviceAccountPath = path.resolve(__dirname, '../../serviceAccountKey.json');
-
-// サービスアカウントキーの読み込み
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+// サービスアカウントキーを環境変数から取得
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY as string);
 
 // Firebase の初期化
 const app = initializeApp({
     credential: cert(serviceAccount),
-    storageBucket: 'udon-9d359.appspot.com' // ストレージバケットを指定
+    storageBucket: process.env.STORAGE_BUCKET, // 環境変数からストレージバケットを指定
 });
 
 const db = getFirestore(app);
